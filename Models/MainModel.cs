44637214c0623bec4ae08dfd79d9416b7ecb9fd0
IvanHattler <45098680+IvanHattler@users.noreply.github.com>
2018-12-16 11:23:45 +0300

    Add files via upload

---
+++ Models/MainModel.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Math;
+
+namespace Lab5.Models
+{
+    class MainModel
+    {
+        public static Tuple<double, double, double> Func(int R, double alfa, double beta)
+        {
+            double x = R * Sin(alfa) * Cos(beta) * (1 + 0.5 * Abs(Sin(2 * beta)));
+            double y = R * Sin(alfa) * Sin(beta) *(1 + 0.5 * Abs(Sin(2 * beta)));
+            double z = R * Cos(alfa); //*Abs(Cos(beta + alfa))
+            return Tuple.Create(x,y,z);
+        }
+        public static Tuple<double, double, double> Axonometry(Tuple<double, double, double> coords, double alfa, double beta)
+        {
+            double x = coords.Item1 * Cos(alfa) + coords.Item2 * Sin(alfa);
+            double y = -coords.Item1 * Sin(alfa) * Cos(beta) + coords.Item2 * Cos(alfa) * Cos(beta) + coords.Item3 * Sin(beta);
+            double z = coords.Item1 * Sin(alfa) * Sin(beta) - coords.Item2 * Cos(alfa) * Sin(beta) + coords.Item3 * Cos(beta);
+            return Tuple.Create(x, y, z);
+        }
+        public static void Line(WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
+        {
+            int lengthX = Math.Abs(x2 - x1);
+            int lengthY = Math.Abs(y2 - y1);
+
+            int dx = (x2 - x1 >= 0 ? 1 : -1);
+            int dy = (y2 - y1 >= 0 ? 1 : -1);
+
+            int length = Math.Max(lengthX, lengthY);
+
+            if (length == 0)
+            {
+                if (x1 < wb.PixelWidth && y1 < wb.PixelHeight && x1 > 0 && y1 > 0)
+                    wb.SetPixel(x1, y1, color);
+            }
+            if (lengthY <= lengthX)
+            {
+                int x = x1;
+                int y = y1;
+                int d = -lengthX;
+                length++;
+                while (length > 0)
+                {
+                    if (x < wb.PixelWidth && y < wb.PixelHeight && x > 0 && y > 0)
+                    wb.SetPixel(x, y, color);
+                    x += dx;
+                    d += 2 * lengthY;
+                    if (d > 0)
+                    {
+                        d -= 2 * lengthX;
+                        y += dy;
+                    }
+                    length--;
+                }
+            }
+            else
+            {
+                int x = x1;
+                int y = y1;
+                int d = -lengthY;
+
+                length++;
+                while (length > 0)
+                {
+                    if (x < wb.PixelWidth && y < wb.PixelHeight && x>0 && y>0)
+                        wb.SetPixel(x, y, color);
+                    y += dy;
+                    d += 2 * lengthX;
+                    if (d > 0)
+                    {
+                        d -= 2 * lengthY;
+                        x += dx;
+                    }
+                    length--;
+                }
+            }
+        }
+        [Obsolete]
+        public static void Line(WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color, ref int[,] Z, int z_value)
+        {
+            int lengthX = Abs(x2 - x1);
+            int lengthY = Abs(y2 - y1);
+
+            int dx = (x2 - x1 >= 0 ? 1 : -1);
+            int dy = (y2 - y1 >= 0 ? 1 : -1);
+
+            int length = Max(lengthX, lengthY);
+
+            if (length == 0)
+            {
+                if (x1 < wb.PixelWidth && y1 < wb.PixelHeight && x1 > 0 && y1 > 0)
+                    wb.SetPixel(x1, y1, color);
+            }
+            if (lengthY <= lengthX)
+            {
+                int x = x1;
+                int y = y1;
+                int d = -lengthX;
+                length++;
+                if (Z[x, y] < z_value)
+                {                    
+                    //Z[x - 1, y - 1] = z_value;
+                    //Z[x, y - 1] = z_value;
+                    //Z[x + 1, y - 1] = z_value;
+
+                    //Z[x-1, y] = z_value;
+                    //Z[x, y] = z_value;
+                    for(int i = 10; i >= 0; i--)
+                    {
+                        for (int j = 10; j >= 0; j--)
+                        {
+                            Z[x-j,y-i] = z_value;
+                        }
+                    }                   
+                    //Z[x + 1, y] = z_value;
+
+                    //Z[x-1, y+1] = z_value;
+                    //Z[x, y+1] = z_value;
+                    //Z[x+1, y+1] = z_value;
+
+                    while (length > 0)
+                    {
+                    
+                        if (x < wb.PixelWidth && y < wb.PixelHeight && x > 0 && y > 0)
+                            wb.SetPixel(x, y, color);
+                        x += dx;
+                        d += 2 * lengthY;
+                        if (d > 0)
+                        {
+                            d -= 2 * lengthX;
+                            y += dy;
+                        }
+                        length--;
+                    }
+                    
+                }
+            }
+            else
+            {
+                int x = x1;
+                int y = y1;
+                int d = -lengthY;
+
+                length++;
+                if (Z[x, y] < z_value)
+                {
+                    //Z[x - 1, y - 1] = z_value;
+                    //Z[x, y - 1] = z_value;
+                    //Z[x + 1, y - 1] = z_value;
+
+                    //Z[x - 1, y] = z_value;
+                    //Z[x, y] = z_value;
+                    for (int i = 10; i >= 0; i--)
+                    {
+                        for (int j = 10; j >= 0; j--)
+                        {
+                            Z[x - j, y - i] = z_value;
+                        }
+                    }
+                    //Z[x + 1, y] = z_value;
+
+                    //Z[x - 1, y + 1] = z_value;
+                    //Z[x, y + 1] = z_value;
+                    //Z[x + 1, y + 1] = z_value;
+
+                    while (length > 0)
+                    {
+                    
+                        if (x < wb.PixelWidth && y < wb.PixelHeight && x > 0 && y > 0)
+                            wb.SetPixel(x, y, color);
+                        y += dy;
+                        d += 2 * lengthX;
+                        if (d > 0)
+                        {
+                            d -= 2 * lengthY;
+                            x += dx;
+                        }
+                        length--;
+                    }
+                }
+            }
+        }
+        public static void Polygon(WriteableBitmap wb, Point a, Point b,Point c,Point d, Color color)
+        {
+            wb.DrawPolyline(new int[] { (int)a.X, (int)a.Y, (int)b.X, (int)b.Y, (int)c.X, (int)c.Y
+            , (int)d.X, (int)d.Y, (int)a.X, (int)a.Y}, color);
+        }
+        public static Tuple<double, double, double> Scaling(Tuple<double, double, double> coords, int k)
+        {
+            double x = coords.Item1 * k;
+            double y = coords.Item2 * k;
+            double z = coords.Item3 * k;
+            return Tuple.Create(x, y, z);
+        }
+        public static Tuple<double, double, double> Moving(Tuple<double, double, double> coords, int dx, int dy,int dz)
+        {
+            double x = coords.Item1 + dx;
+            double y = coords.Item2 + dy;
+            double z = coords.Item3 + dz;
+            return Tuple.Create(x, y, z);
+        }
+        public static Tuple<double, double, double> RotationX(Tuple<double, double, double> coords, int angle)
+        {
+            double x = coords.Item1;
+            double y = coords.Item2 * Cos(angle) - coords.Item3 * Sin(angle);
+            double z = coords.Item2 * Sin(angle) + coords.Item3 * Cos(angle);
+            return Tuple.Create(x, y, z);
+        }
+        public static Tuple<double, double, double> RotationY(Tuple<double, double, double> coords, int angle)
+        {
+            double x = coords.Item1* Cos(angle) + coords.Item3 * Sin(angle);
+            double y = coords.Item2;
+            double z = -coords.Item1 * Sin(angle) + coords.Item3 * Cos(angle);
+            return Tuple.Create(x, y, z);
+        }
+        public static Tuple<double, double, double> RotationZ(Tuple<double, double, double> coords, int angle)
+        {
+            double x = coords.Item1 * Cos(angle) - coords.Item2 * Sin(angle);
+            double y = coords.Item1 * Sin(angle) + coords.Item2* Cos(angle);
+            double z = coords.Item3;
+            return Tuple.Create(x, y, z);
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using Lab5.Commands;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Forms;
+using Lab5.Models;
+using System.Windows;
+
+namespace Lab5.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private double alfa = 45;
+        private double beta = 45;
+        private WriteableBitmap wb;
+        private Color color = Color.FromRgb(0, 0, 0);
+        private int r = 200;
+        private int alfaOt = 0;
+        private int betaOt = 0;
+        private int alfaDo = 180;
+        private int betaDo = 360;
+        private int resolution = 3;
+        private Point pointHorizontal = new Point(double.NegativeInfinity, double.NegativeInfinity);
+        private string[] viewTypes = new string[] { "Draw full carcass", "Draw horizontal carcass" };
+        private int selectedIndex = 1;
+        private int affineAngleX = 0;
+        private int affineAngleY = 0;
+        private int affineAngleZ = 0;
+        private int dx = 0;
+        private int dy = 0;
+        private int dz = 0;
+        private int k = 1;
+        public int SelectedIndex
+        {
+            get { return selectedIndex; }
+            set
+            {
+                selectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        public string[] ViewTypes
+        {
+            get { return viewTypes; }
+            set
+            {
+                viewTypes = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// расстоянние между горизонтальными линиями в пикселях
+        /// </summary>
+        public int Resolution
+        {
+            get { return resolution; }
+            set
+            {
+                resolution = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// начало интервала угла альфа фигуры
+        /// </summary>
+        public int AlfaOt
+        {
+            get { return alfaOt; }
+            set
+            {
+                alfaOt = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// начало интервала угла бета фигуры
+        /// </summary>
+        public int BetaOt
+        {
+            get { return betaOt; }
+            set
+            {
+                betaOt = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// конец интервала угла альфа фигуры
+        /// </summary>
+        public int AlfaDo
+        {
+            get { return alfaDo; }
+            set
+            {
+                alfaDo = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// конец интервала угла бета фигуры
+        /// </summary>
+        public int BetaDo
+        {
+            get { return betaDo; }
+            set
+            {
+                betaDo = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// ро фигуры
+        /// </summary>
+        public int R
+        {
+            get { return r; }
+            set
+            {
+                r = value;
+                OnPropertyChanged();
+            }
+        }
+        public WriteableBitmap Wb
+        {
+            get { return wb; }
+            set
+            {
+                wb = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// угол альфа камеры
+        /// </summary>
+        public double Alfa
+        {
+            get { return alfa; }
+            set
+            {
+                alfa = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// угол бета камеры
+        /// </summary>
+        public double Beta
+        {
+            get { return beta; }
+            set
+            {
+                beta = value;
+                OnPropertyChanged();
+            }
+        }
+        public int AffineAngleX
+        {
+            get
+            {
+                return affineAngleX;
+            }
+
+            set
+            {
+                affineAngleX = value;
+                OnPropertyChanged();
+            }
+        }
+        public int AffineAngleY
+        {
+            get
+            {
+                return affineAngleY;
+            }
+
+            set
+            {
+                affineAngleY = value;
+                OnPropertyChanged();
+            }
+        }
+        public int AffineAngleZ
+        {
+            get
+            {
+                return affineAngleZ;
+            }
+
+            set
+            {
+                affineAngleZ = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Dx
+        {
+            get
+            {
+                return dx;
+            }
+
+            set
+            {
+                dx = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Dy
+        {
+            get
+            {
+                return dy;
+            }
+
+            set
+            {
+                dy = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Dz
+        {
+            get
+            {
+                return dz;
+            }
+
+            set
+            {
+                dz = value;
+                OnPropertyChanged();
+            }
+        }
+        public int K
+        {
+            get
+            {
+                return k;
+            }
+
+            set
+            {
+                k = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            wb = BitmapFactory.New(1000, 725);
+        }
+
+        #region Commands
+        public DelegateCommand drawCommand;
+        public DelegateCommand Draw
+        {
+            get
+            {
+                return drawCommand ?? (drawCommand = new DelegateCommand(obj =>
+                {
+                    switch (SelectedIndex)
+                    {
+                        case 0:
+                            {
+                                DrawFullCarcass();
+                                break;
+                            }
+                        case 1:
+                            {
+                                DrawHorizontalCarcass();
+                                break;
+                            }
+                        case 2:
+                            {
+                                DrawPolygons();
+                                break;
+                            }
+                    }
+                }));
+            }
+        }
+        private DelegateCommand clearCommand;
+        public DelegateCommand Clear
+        {
+            get
+            {
+                return clearCommand ?? (clearCommand = new DelegateCommand(obj =>
+                {
+                    Wb.Clear();
+                }));
+            }
+        }
+        private DelegateCommand colorCommand;
+        public DelegateCommand ColorCommand
+        {
+            get
+            {
+                return colorCommand ?? (colorCommand = new DelegateCommand(obj =>
+                {
+                    var colorDial = new ColorDialog();
+                    if (colorDial.ShowDialog() == DialogResult.OK)
+                        color = Color.FromRgb(colorDial.Color.R, colorDial.Color.G, colorDial.Color.B);
+                    colorDial = null;
+
+
+                }));
+            }
+        }
+        private DelegateCommand applyAffineCommand;
+        public DelegateCommand ApplyAffine
+        {
+            get
+            {
+                return applyAffineCommand ?? (applyAffineCommand = new DelegateCommand(obj =>
+                {
+                    Wb.Clear();
+                    DrawHorizontalCarcass(MainModel.Scaling, MainModel.Moving, MainModel.RotationX, MainModel.RotationY, MainModel.RotationZ);
+                }));
+            }
+        }       
+
+
+        #endregion
+        public void DrawFullCarcass()
+        {
+            Wb.Clear();
+
+            ////Z - buffer
+            //int[,] Z = new int[Wb.PixelWidth, Wb.PixelHeight];
+            //for (int i = 0; i < Wb.PixelWidth; i++)
+            //{
+            //    for (int j = 0; j < Wb.PixelHeight; j++)
+            //    {
+            //        Z[i, j] = Int32.MinValue;
+            //    }
+            //}
+            Point[] buffer = new Point[(BetaDo - BetaOt)];
+            for (int alfa = AlfaOt; alfa < AlfaDo; alfa += resolution)
+            {
+
+                for (int beta = BetaOt; beta < BetaDo; beta += resolution*2)
+                {
+                    var res = MainModel.Axonometry(MainModel.Func(R, alfa * Math.PI / 180, beta * Math.PI / 180),
+                        Alfa * Math.PI / 180,
+                        Beta * Math.PI / 180);
+                    if (!double.IsNegativeInfinity(pointHorizontal.X))
+                    {
+                        MainModel.Line(wb, (int)pointHorizontal.X, (int)pointHorizontal.Y,
+                        (int)res.Item1 + Wb.PixelWidth / 2, (int)res.Item2 + Wb.PixelHeight / 2,
+                         color);// ,ref Z, (int)res.Item3);
+                        if (alfa > 0)
+                            MainModel.Line(wb, (int)buffer[beta].X, (int)buffer[beta].Y,
+                            (int)res.Item1 + Wb.PixelWidth / 2, (int)res.Item2 + Wb.PixelHeight / 2,
+                            color);
+                    }
+                    buffer[beta] = new Point(res.Item1 + Wb.PixelWidth / 2, res.Item2 + Wb.PixelHeight / 2);
+                    if (alfa < AlfaDo - resolution)
+                        pointHorizontal = new Point(res.Item1 + Wb.PixelWidth / 2, res.Item2 + Wb.PixelHeight / 2);
+                    else
+                        pointHorizontal.X = double.NegativeInfinity;
+                }
+            }
+        }
+        public void DrawHorizontalCarcass()
+        {
+            Wb.Clear();
+
+            ////Z - buffer
+            //int[,] Z = new int[Wb.PixelWidth, Wb.PixelHeight];
+            //for (int i = 0; i < Wb.PixelWidth; i++)
+            //{
+            //    for (int j = 0; j < Wb.PixelHeight; j++)
+            //    {
+            //        Z[i, j] = Int32.MinValue;
+            //    }
+            //}
+            for (int alfa = AlfaOt; alfa < AlfaDo; alfa += resolution)
+            {
+
+                for (int beta = BetaOt; beta < BetaDo; beta += resolution)
+                {
+                    var res = MainModel.Axonometry(MainModel.Func(R, alfa * Math.PI / 180, beta * Math.PI / 180),
+                        Alfa * Math.PI / 180,
+                        Beta * Math.PI / 180);
+                    if (!double.IsNegativeInfinity(pointHorizontal.X))
+                    {
+                        MainModel.Line(wb, (int)pointHorizontal.X, (int)pointHorizontal.Y,
+                        (int)res.Item1 + Wb.PixelWidth / 2, (int)res.Item2 + Wb.PixelHeight / 2,
+                         color);//,ref Z, (int)res.Item3);                               
+                    }
+                    if (alfa < AlfaDo - resolution)
+                        pointHorizontal = new Point(res.Item1 + Wb.PixelWidth / 2, res.Item2 + Wb.PixelHeight / 2);
+                    else
+                        pointHorizontal.X = double.NegativeInfinity;
+                }
+            }
+        }
+        public void DrawPolygons()
+        {
+            Wb.Clear();
+            Point[] bufferUp = new Point[(BetaDo - BetaOt)];
+            Point[] bufferDown = new Point[(BetaDo - BetaOt)];
+            for (int alfa = AlfaOt; alfa < AlfaDo; alfa += resolution)
+            {
+                for (int beta = BetaOt; beta < BetaDo; beta += resolution)
+                {
+                    var res = MainModel.Axonometry(MainModel.Func(R, alfa * Math.PI / 180, beta * Math.PI / 180),
+                       Alfa * Math.PI / 180,
+                       Beta * Math.PI / 180);
+                    bufferDown[beta] = new Point(res.Item1 + Wb.PixelWidth / 2, res.Item2 + Wb.PixelHeight / 2);
+                    if (alfa > 1 && beta > 0)
+                        MainModel.Polygon(wb, bufferUp[beta - 1], bufferUp[beta], bufferDown[beta - 1], bufferDown[beta],color);
+                    if (alfa > 0 && beta > 0)
+                        bufferUp[beta-1] = bufferDown[beta-1];
+                }
+            }
+        }
+        public void DrawHorizontalCarcass(Func<Tuple<double, double, double>,int, Tuple<double, double, double>> scaling,
+            Func<Tuple<double, double, double>,int, int, int, Tuple<double, double, double>> moving,
+            Func<Tuple<double, double, double>, int, Tuple<double, double, double>> rX,
+            Func<Tuple<double, double, double>, int, Tuple<double, double, double>> rY,
+            Func<Tuple<double, double, double>, int, Tuple<double, double, double>> rZ)
+        {
+            Wb.Clear();
+
+            ////Z - buffer
+            //int[,] Z = new int[Wb.PixelWidth, Wb.PixelHeight];
+            //for (int i = 0; i < Wb.PixelWidth; i++)
+            //{
+            //    for (int j = 0; j < Wb.PixelHeight; j++)
+            //    {
+            //        Z[i, j] = Int32.MinValue;
+            //    }
+            //}
+            for (int alfa = AlfaOt; alfa < AlfaDo; alfa += resolution)
+            {
+
+                for (int beta = BetaOt; beta < BetaDo; beta += resolution * 2)
+                {
+                    var res = MainModel.Axonometry(rZ(rY(rX(moving(scaling(MainModel.Func(R, alfa * Math.PI / 180, beta * Math.PI / 180),K),Dx,Dy,Dz),AffineAngleX),AffineAngleY),AffineAngleZ),
+                        Alfa * Math.PI / 180,
+                        Beta * Math.PI / 180);
+                    if (!double.IsNegativeInfinity(pointHorizontal.X))
+                    {
+                        MainModel.Line(wb, (int)pointHorizontal.X, (int)pointHorizontal.Y,
+                        (int)res.Item1 + Wb.PixelWidth / 2, (int)res.Item2 + Wb.PixelHeight / 2,
+                         color);//,ref Z, (int)res.Item3);                               
+                    }
+                    if (alfa < AlfaDo - resolution)
+                        pointHorizontal = new Point(res.Item1 + Wb.PixelWidth / 2, res.Item2 + Wb.PixelHeight / 2);
+                    else
+                        pointHorizontal.X = double.NegativeInfinity;
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
