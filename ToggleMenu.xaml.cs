a2b919ac997b3300b62af02eaaab1bfe345402e1
IvanHattler <45098680+IvanHattler@users.noreply.github.com>
2018-12-16 11:24:56 +0300

    Add files via upload

---
+++ ToggleMenu.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Interaction logic for ToggleMenu.xaml
+    /// </summary>
+    public partial class ToggleMenu : UserControl
+    {
+        public int R
+        {
+            get { return (int)GetValue(RProperty); }
+            set { SetValue(RProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for R.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RProperty =
+            DependencyProperty.Register("R", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+
+        public int AlfaOt
+        {
+            get { return (int)GetValue(AlfaOtProperty); }
+            set { SetValue(AlfaOtProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AlfaOt.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlfaOtProperty =
+            DependencyProperty.Register("AlfaOt", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+
+        public int AlfaDo
+        {
+            get { return (int)GetValue(AlfaDoProperty); }
+            set { SetValue(AlfaDoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AlfaDo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlfaDoProperty =
+            DependencyProperty.Register("AlfaDo", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+
+        public int BetaOt
+        {
+            get { return (int)GetValue(BetaOtProperty); }
+            set { SetValue(BetaOtProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BetaOt.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BetaOtProperty =
+            DependencyProperty.Register("BetaOt", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+
+        public int BetaDo
+        {
+            get { return (int)GetValue(BetaDoProperty); }
+            set { SetValue(BetaDoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BetaDo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BetaDoProperty =
+            DependencyProperty.Register("BetaDo", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+        public int Resolution
+        {
+            get { return (int)GetValue(ResolutionProperty); }
+            set { SetValue(ResolutionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Resolution.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ResolutionProperty =
+            DependencyProperty.Register("Resolution", typeof(int), typeof(ToggleMenu), new PropertyMetadata(0));
+
+
+
+
+        public ToggleMenu()
+        {
+            InitializeComponent();
+        }
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
---
+++ ToggleMenu1.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Interaction logic for ToggleMenu1.xaml
+    /// </summary>
+    public partial class ToggleMenu1 : UserControl
+    {
+
+        public int AffineAngleX
+        {
+            get { return (int)GetValue(AffineAngleXProperty); }
+            set { SetValue(AffineAngleXProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Beta.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AffineAngleXProperty =
+            DependencyProperty.Register("AffineAngleX", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+        public int AffineAngleY
+        {
+            get { return (int)GetValue(AffineAngleYProperty); }
+            set { SetValue(AffineAngleYProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Alfa.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AffineAngleYProperty =
+            DependencyProperty.Register("AffineAngleY", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+        public int AffineAngleZ
+        {
+            get { return (int)GetValue(AffineAngleZProperty); }
+            set { SetValue(AffineAngleZProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AffineAngleZ.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AffineAngleZProperty =
+            DependencyProperty.Register("AffineAngleZ", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+
+
+        public int Dz
+        {
+            get { return (int)GetValue(DzProperty); }
+            set { SetValue(DzProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Dz.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DzProperty =
+            DependencyProperty.Register("Dz", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+
+
+        public int Dy
+        {
+            get { return (int)GetValue(DyProperty); }
+            set { SetValue(DyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Dy.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DyProperty =
+            DependencyProperty.Register("Dy", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+
+        public int Dx
+        {
+            get { return (int)GetValue(DxProperty); }
+            set { SetValue(DxProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Dx.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DxProperty =
+            DependencyProperty.Register("Dx", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(0));
+
+
+        public int K
+        {
+            get { return (int)GetValue(KProperty); }
+            set { SetValue(KProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for R.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty KProperty =
+            DependencyProperty.Register("K", typeof(int), typeof(ToggleMenu1), new PropertyMetadata(1));
+
+        public ToggleMenu1()
+        {
+            InitializeComponent();
+        }
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
